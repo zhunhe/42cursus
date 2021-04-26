817373bea6d939db1a24da173f654b4626978a53
zhunhe <xuzhunhe@gmail.com>
2021-04-26 23:18:34 +0900

    Init add libft.h

---
+++ libft/incl/libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/25 00:46:25 by juhur             #+#    #+#             */
+/*   Updated: 2021/04/26 22:51:45 by juhur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+size_t				ft_strlen(char *s);
+void				*ft_memset(void *b, int c, size_t len);
+#endif
