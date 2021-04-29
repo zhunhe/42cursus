5b7ae812ee43fc27a39b340171910afb7b67fc2f
zhunhe <xuzhunhe@gmail.com>
2021-04-29 14:59:21 +0900

    Init add ft_memcpy.c

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/29 14:08:22 by juhur             #+#    #+#             */
+/*   Updated: 2021/04/29 14:57:26 by juhur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** memcpy -- copy memory area (string.h)
+*/
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t idx;
+
+	if (!dst && !src)
+		return (NULL);
+	if (!n || !*(unsigned char *)src)
+		return (dst);
+	idx = 0;
+	while (idx < n)
+	{
+		if (*(unsigned char *)(src + idx))
+			*(unsigned char *)(dst + idx) = *(unsigned char *)(src + idx);
+		else
+			*(unsigned char *)(dst + idx) = 0;
+		++idx;
+	}
+	return (dst);
+}
