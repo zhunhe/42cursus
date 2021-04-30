cddba25aa89ca107d4ea9b0406a971aaac090f85
zhunhe <xuzhunhe@gmail.com>
2021-04-30 13:35:56 +0900

    Modify ft_memcmp.c remove curly braces in ft_memcmp()

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 13:06:30 by juhur             #+#    #+#             */
-/*   Updated: 2021/04/30 13:32:55 by juhur            ###   ########.fr       */
+/*   Updated: 2021/04/30 13:34:08 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	idx = 0;
 	while ((idx < n - 1)
 	&& (*(unsigned char *)(s1 + idx) == *(unsigned char *)(s2 + idx)))
-	{
 		++idx;
-	}
 	return (*(unsigned char *)(s1 + idx) - *(unsigned char *)(s2 + idx));
 }
